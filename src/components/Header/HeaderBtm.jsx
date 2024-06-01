10be42f80b1a748422c77f90f2de7e88dff21333
WeBstwr <ifedhawebster@gmail.com>
2024-06-01 11:23:43 +0300

    Create the header bottom
    I have created the botto part of my header
    
    I have also made my header responsive

--- src/components/Header/HeaderBtm.jsx
+++ src/components/Header/HeaderBtm.jsx
@@ -1,5 +1,26 @@
+import "./header.css";
+
 const HeaderBtm = () => {
-  return <div className="header-btm">header bottom</div>;
+  return (
+    <div className="header-btm">
+      <nav className="header-nav">
+        <ol className="header-nav-list">
+          <li className="header-nav-item">
+            <a href="">home</a>
+          </li>
+          <li className="header-nav-item">
+            <a href="">leadership</a>
+          </li>
+          <li className="header-nav-item">
+            <a href="">tracks</a>
+          </li>
+          <li className="header-nav-item">
+            <a href="">events</a>
+          </li>
+        </ol>
+      </nav>
+    </div>
+  );
 };
 
 export default HeaderBtm;
--- src/components/Header/HeaderTop.jsx
+++ src/components/Header/HeaderTop.jsx
@@ -2,25 +2,25 @@ import { MdEmail } from "react-icons/md";
 import { IoLogoWhatsapp } from "react-icons/io";
 import "./header.css";
 
-const Socials = ({icon, label}) =>{
-  return(
+const Socials = ({ icon, label }) => {
+  return (
     <div className="socials">
-      <div className="socials-icon-wrapper">
-        {icon}
-      </div>
+      <div className="socials-icon-wrapper">{icon}</div>
       {label}
     </div>
-  )
-}
+  );
+};
 
 const HeaderTop = () => {
-  return <div className="header-top">
-    <h1 className="logo">MUT.CLUB</h1>
-    <div className="header-social">
-      <Socials icon={<MdEmail />} label={"info@mutc.com"} />
-      <Socials icon={<IoLogoWhatsapp />} label={"+254715652746"} />
+  return (
+    <div className="header-top">
+      <h1 className="logo">MUT.CLUB</h1>
+      <div className="header-social">
+        <Socials icon={<MdEmail />} label={"info@mutc.com"} />
+        <Socials icon={<IoLogoWhatsapp />} label={"+254715652746"} />
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default HeaderTop;
