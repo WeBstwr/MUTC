3ca23977a1833f237f61104d48352ad2aab95d23
WeBstwr <ifedhawebster@gmail.com>
2024-06-01 10:07:29 +0300

    Create the top header
    I have created the top header of the page.
    
    I have added the icons for the email and whatsapp contact.

--- src/components/Header/HeaderTop.jsx
+++ src/components/Header/HeaderTop.jsx
@@ -1,5 +1,26 @@
+import { MdEmail } from "react-icons/md";
+import { IoLogoWhatsapp } from "react-icons/io";
+import "./header.css";
+
+const Socials = ({icon, label}) =>{
+  return(
+    <div className="socials">
+      <div className="socials-icon-wrapper">
+        {icon}
+      </div>
+      {label}
+    </div>
+  )
+}
+
 const HeaderTop = () => {
-  return <div className="header-top">header top</div>;
+  return <div className="header-top">
+    <h1 className="logo">MUT.CLUB</h1>
+    <div className="header-social">
+      <Socials icon={<MdEmail />} label={"info@mutc.com"} />
+      <Socials icon={<IoLogoWhatsapp />} label={"+254715652746"} />
+    </div>
+  </div>;
 };
 
 export default HeaderTop;
