563a77f6d59d68bfddfc520141a08ecf9efc4a49
WeBstwr <ifedhawebster@gmail.com>
2024-06-02 15:50:31 +0300

    add a banner to the pages
    I have created a banner to each page apart from the home page.

--- src/pages/Tracks/Tracks.jsx
+++ src/pages/Tracks/Tracks.jsx
@@ -1,5 +1,15 @@
+import Banner from "../../components/Banner/Banner";
+
 const Tracks = () => {
-  return <h2>Tracks</h2>;
+  return (
+    <section>
+      <Banner
+        mainText="our leaders"
+        subText="
+Welcome to MUTC's leadership page. Meet our passionate leaders driving innovation, fostering learning, and empowering our tech community."
+      />
+    </section>
+  );
 };
 
 export default Tracks;
